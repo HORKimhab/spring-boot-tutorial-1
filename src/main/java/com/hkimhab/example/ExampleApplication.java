9f242cdbf5eb70ace83735516f3023dcdbbea92a
HORKimhab <kimhab.zeecode@gmail.com>
2026-01-30 23:32:31 +0700

    lesson: :memo: decorate bean - @bean

--- src/main/java/com/hkimhab/example/ExampleApplication.java
+++ src/main/java/com/hkimhab/example/ExampleApplication.java
@@ -2,12 +2,29 @@ package com.hkimhab.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExampleApplication.class, args);
+		// SpringApplication.run(ExampleApplication.class, args);
+
+		// MyFirstClass myFirstClass = new MyFirstClass();
+		// System.out.println(myFirstClass.sayHello());
+
+
+		// By Bean 
+		var ctx = SpringApplication.run(ExampleApplication.class, args);
+
+		MyFirstClass myFirstClass = ctx.getBean(MyFirstClass.class); 
+		System.out.println(myFirstClass.sayHello());
+	}
+
+	// By Bean 
+	@Bean 
+	public MyFirstClass myFirstClass(){
+		return new MyFirstClass();
 	}
 
 }
---
+++ src/main/java/com/hkimhab/example/MyFirstClass.java
@@ -0,0 +1,12 @@
+package com.hkimhab.example;
+
+/**
+ *
+ * @author H.Kimhab
+ */
+public class MyFirstClass {
+  
+  public String sayHello() {
+    return "Hello Universe from the MyFirstClass";
+  }
+}
